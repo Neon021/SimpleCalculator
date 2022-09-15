cb337b14ceac398fa1b56bcd2eaa6cd09e2d0f8a
Furkan Bilal <bilalyigit594@gmail.com>
2022-09-15 22:49:25 +0300

    init

---
+++ Calculator1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class Calculator1
+    {
+        public static class _Calculator
+        {
+            public class Operators
+            {
+
+                public void Addition(int x, int y)
+                {
+                    int z = x + y;
+                    Console.WriteLine(z);
+                }
+
+                public void Multiplication(int x, int y)
+                {
+                    int z = x * y;
+                    Console.WriteLine(z);
+                }
+
+                public void Division(int x, int y)
+                {
+                    decimal z = x / y;
+                    Console.WriteLine(z);
+                }
+            }
+
+            public class SimpleCalculator
+            {
+                public void Calculate(int x, int y, string op)
+                {
+                    Operators p = new Operators();
+
+                    if (op == "Addition")
+                    {
+                        p.Addition(x, y);
+                    }
+                    else if (op == "Multiplication")
+                    {
+                        p.Multiplication(x, y);
+                    }
+                    else if (op == "Division")
+                    {
+                        if (x == 0)
+                        {
+                            try { }
+                            catch (DivideByZeroException)
+                            {
+                                Console.WriteLine("You can't divide by zero");
+                            }
+                        }
+                        else
+                            p.Division(x, y);
+                    }
+                    else if (op == null || op == "" || op == "Subtraction")
+                    {
+                        Console.WriteLine("Check the operator");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Calculator2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    public class Calculator2
+    {
+        public static class _Calculator
+        {
+            public static string Calculator(int operand1, int operand2, string operation)
+            {
+                return operation switch
+                {
+                    "+" => $"{operand1} {operation} {operand2} = {operand1 + operand2}",
+                    "*" => $"{operand1} {operation} {operand2} = {operand1 * operand2}",
+                    "/" when operand2 == 0 => "Division by zero is not allowed.",
+                    "/" => $"{operand1} {operation} {operand2} = {operand1 / operand2}",
+                    null => throw new ArgumentNullException(),
+                    "" => throw new ArgumentException(),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }
+        }
+
+
+    }
+}
